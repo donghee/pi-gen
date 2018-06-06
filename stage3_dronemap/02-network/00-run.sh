746ab9400053fcb458d4359ea008d9018f111630
Donghee Park <dongheepark@gmail.com>
2018-06-06 11:26:56 +0900

    update rt8812au v4.3.21

--- stage3_dronemap/02-network/00-run.sh
+++ stage3_dronemap/02-network/00-run.sh
@@ -14,22 +14,22 @@ install -m 644 files/lte-qmi.sh ${ROOTFS_DIR}/usr/local/bin
 cp -r files ${ROOTFS_DIR}/files
 
 on_chroot << EOF
-dpkg -i /files/*.deb
+#dpkg -i /files/*.deb
 
 #bash /files/realtek-rt8812au_emlid
 #/files/rpi-source -q --tag-update
 #/files/rpi-source --skip-gcc
 
-#git clone https://github.com/emlid/8812au.git
-#cd 8812au
-#git checkout v4.3.21
-#make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm
-#sudo insmod 8812au.ko
-#sudo cp 8812au.ko /lib/modules/4.14.34-v7+/kernel/drivers/net/wireless
-#sudo depmod
-#cp rtl8812-txpower.rules /etc/udev/rules.d/rtl8812-txpower.rules
-#cd ..
-#rm -rf linux*
+git clone https://github.com/emlid/8812au.git
+cd 8812au
+git checkout v4.3.21
+make CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm
+sudo insmod 8812au.ko
+sudo cp 8812au.ko /lib/modules/4.14.34-v7+/kernel/drivers/net/wireless
+sudo depmod
+cp rtl8812-txpower.rules /etc/udev/rules.d/rtl8812-txpower.rules
+cd ..
+rm -rf linux*
 
 EOF
 rm -rf ${ROOTFS_DIR}/files
